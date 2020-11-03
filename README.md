9eda82901e50af10e9c7398d6ff27b31ac7ffa91
Stareven233 <1429960342@qq.com>
2020-11-03 22:44:37 +0800

    feat: add live2d in newtab

--- README.md
+++ README.md
@@ -10,3 +10,10 @@
 ### 自定义
 - 修改目录下`manifest.json`定义程序信息
 - 修改newtab.html即可对应修改新标签页
+
+### 踩坑
+现今(2020.11.3) chrome的CSP限制非常严格，几乎不允许内联js执行
+参考[stackoverflow相关问题](https://stackoverflow.com/questions/25625412chrome-extension-content-security-policy-executing-inline-code)  
+测试过 "script-src 'unsafe-inline';" / "script-src *;" / "script-src nonce-xxxx"等发现皆已失效，而sha256似乎也行不通  
+最后设置CSP为 "script-src 'self' https://xxx.com;" 
+通过引用本地js及网络资源才成功，其中网址必须要带协议部分，仅主机无法使用  
---
+++ js/2233live2d.js
@@ -0,0 +1,39 @@
+// 白猫
+// https://unpkg.com/live2d-widget-model-tororo@1.0.5/assets/tororo.model.json
+// 黑猫
+// https://unpkg.com/live2d-widget-model-tororo@1.0.5/assets/tororo.model.json
+// bilibili 33
+// https://raw.githubusercontent.com/Stareven233/static/master/live2d/Bilibili2233/haruna/33/model.2018.bls-winter.json
+
+function rand_choice(arr) {
+return arr[Math.round(Math.random()*(arr.length-1))];
+}
+
+let haruna_basedir = "https://raw.githubusercontent.com/Stareven233/static/master/live2d/Bilibili2233/haruna/"
+let haruna = ["22", "33"];
+let closet_list = ['model.2016.xmas.1.json', 'model.2016.xmas.2.json', 'model.2017.cba-normal.json', 'model.2017.cba-super.json', 'model.2017.newyear.json', 'model.2017.school.json', 'model.2017.summer.normal.1.json', 'model.2017.summer.normal.2.json', 'model.2017.summer.super.1.json', 'model.2017.summer.super.2.json', 'model.2017.tomo-bukatsu.high.json', 'model.2017.tomo-bukatsu.low.json', 'model.2017.valley.json', 'model.2017.vdays.json', 'model.2018.bls-summer.json', 'model.2018.bls-winter.json', 'model.2018.lover.json', 'model.2018.spring.json', 'model.default.json'];
+let model_path = haruna_basedir;
+model_path += rand_choice(haruna) + '/' + rand_choice(closet_list);
+// console.log(model_path);
+
+　　L2Dwidget.init({
+    "model": {
+        "jsonPath": model_path,
+        "scale": 1 
+    }, 
+    "display": { 
+        "position": "right", 
+        "width": 120, 
+        "height": 160,
+    　 "hOffset": 0, 
+        "vOffset": 70
+    }, 
+    "mobile": { 
+        "show": true, 
+        "scale": 0.5 
+    },
+    　"react": { 
+        "opacityDefault": 1, 
+        "opacityOnHover": 0.2 
+    } 
+    });
